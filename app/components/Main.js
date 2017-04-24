9791e01dd5c5fd7bb938f4727588d87caca73f79
Dino Arachchi <Dino Arachchi>
2017-04-23 23:47:30 -0500

    Added buttons for login/signup

--- app/components/Main.js
+++ app/components/Main.js
@@ -2,49 +2,28 @@
 import React, {Component} from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import RaisedButton from 'material-ui/RaisedButton';
 
 // Including the Link component from React Router to navigate within our application without full page reloads
 var Link = require("react-router").Link;
 
 // Create the Main component
 class Main extends Component {
-
 	render() {
-
 		return (
 			// We can only render a single div. So we need to group everything inside of this main-container one
 			<MuiThemeProvider>
 				<div className="main-container">
 					<div className="container">
-						{/* Navbar */}
-						<nav className="navbar navbar-default" role="navigation">
-							<div className="container-fluid">
-								<div className="navbar-header">
-									<button
-										type="button"
-										className="navbar-toggle"
-										data-toggle="collapse"
-										data-target=".navbar-ex1-collapse"
-									>
-										<span className="sr-only">Toggle navigation</span>
-										<span className="icon-bar"></span>
-										<span className="icon-bar"></span>
-										<span className="icon-bar"></span>
-									</button>
-									<Link className="navbar-brand" to="/">Star Car Finder</Link>
-								</div>
-
-								<div className="collapse navbar-collapse navbar-ex1-collapse">
-									<ul className="nav navbar-nav navbar-right">
-									</ul>
-								</div>
-							</div>
-						</nav>
 
 						{/* Jumbotron */}
 						<div className="jumbotron">
 							<h1 className="text-center"><strong>Star Car Finder</strong></h1>
 							<h2 className="text-center">Search for a Vehicle for Your Next Film</h2>
+									<div>
+										<RaisedButton label="Sign Up" primary={true} />
+										<RaisedButton label="Login" primary={true} />
+									</div>
 						</div>
 
 
