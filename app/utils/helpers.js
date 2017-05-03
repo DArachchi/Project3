a92c697319bb8766d243e01c019bf236d8e96c90
Dino Arachchi <Dino Arachchi>
2017-05-02 21:34:02 -0500

    Added make and model data to database and created select-fields for entering new vehicles

--- app/utils/helpers.js
+++ app/utils/helpers.js
@@ -3,19 +3,39 @@ import axios from 'axios';
 
 // Helper Functions
 const helpers = {
+	// This will return all choices for makes from our database
+	getMakes() {
+		return axios.get("/api/makes")
+		.then(function(response) {
+			return response;
+		});
+	},
+	//This will return all choices for models from our database
+	getModels(makeIndex) {
+		var queryUrl = "/api/models/" + makeIndex;	
+		return axios.get(queryUrl)
+			.then(function(response) {
+			return response;
+		});
+	},
 	// This will return all vehicles from our database
 	getVehicles() {
 		return axios.get("/api/vehicles")
-			.then(function(results) {
-				return results;
-			});
+		.then(function(response) {
+			return response;
+		});
 	},
 	// This will save new vehicles to our database
 	addVehicle(make, model, year, color) {
-		var newVehicle = { make: make, model: model, year: year, color: color};
+		var newVehicle = {
+			make: make,
+			model: model,
+			year: year,
+			color: color
+		};
+
 		return axios.post("/api/vehicles", newVehicle)
 		.then(function(response) {
-			console.log("axios results", response.data);
 			return response.data;
 		});
 	},
